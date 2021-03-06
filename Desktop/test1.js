ba540ab010f1e33ddf51a3ed2bfdd074e5efd0f7
Katarina Jaksic <kacajaksic@macbook-air-od-katarina.home>
2021-03-06 11:51:14 +0100

    first task

---
+++ Desktop/test1.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var path = process.argv[2];
+var validWords = splitWords(fs.readFileSync(path, "utf-8"));
+var validWordsSet=new Set(validWords);
+
+readStdin(function (input) {
+  var inputWords = splitWords(input);
+  inputWords.forEach(function (inputWord) {
+    if (validWordsSet.has(inputWord)) {
+      console.log(inputWord);
+    } else {
+      console.log("<"+inputWord+">");
+    }
+  });
+});
+
+function splitWords(str) {
+  return str.split(/\s+/).filter(function (w) {
+    return w.length > 0;
+  });
+}
+
+function readStdin(cb) {
+  var data = "";
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+      data += chunk;
+    }
+  });
+  process.stdin.on('end', function() {
+    cb(data);
+  });
+}
