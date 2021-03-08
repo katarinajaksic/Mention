af0d6c3143f1c3c31cfa188b7b081ff60bfd9079
katarinajaksic <80153849+katarinajaksic@users.noreply.github.com>
2021-03-08 19:05:35 +0100

    update for env

--- Second/App.js
+++ Second/App.js
@@ -2,8 +2,9 @@ import React, {useEffect, useState}  from 'react';
 import './App.css';
 import Mention from './Mention';
 
-const account_id = "661072_53ca2jsh01c88c4wwkc0wockckk0w4440o4o0w8wkkgco4o888";
-const alert_id = "1214654";
+const account_id = process.env.alert_id;
+const alert_id = process.env.account_id;
+const access_token = process.env.account_id;
 
 const App = () => {
 
@@ -15,7 +16,7 @@ const App = () => {
   
   const getData = async () => {
   
-    const response = await fetch(`https://web.mention.com/api/accounts/${account_id}/alerts/${alert_id}/mentions?access_token=ZDdmNDVmYzU1NWZkMDkwMDc4YjBjMzYyZDk2MDI3NGVlNmFmNTJkZDU5MzBhYWRiZGZmNzAxOGM1NDkzNDYxYQ`);
+    const response = await fetch(`https://web.mention.com/api/accounts/${account_id}/alerts/${alert_id}/mentions?access_token=${access_token}`);
     const data = await response.json();
     console.log(data);
     setMentions(data.mentions);
